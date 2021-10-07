d3c44aecbb376b948bbc8dcdd74bf9d350665ba0
LachlanWIT <52726021+LachlanWIT@users.noreply.github.com>
2021-10-07 12:34:42 +1000

    Lachies Requirement V1

--- lachietest.py
+++ lachietest.py
@@ -1,8 +1,27 @@
 #Test scenarios for functionaln requirements - pending for jack to add to GUI
 
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import date
 
-with open('penalty_data_set_2.csv', newline='') as File:
-    reader = csv.reader(File)
-    for row in reader:
-        print(row)
+#import data
+data = pd.read_csv('penalty_data_set_2.csv', low_memory=False)
+
+
+
+#For a user-selected period, 
+#retrieve all cases captured by radar or camera based on offence 
+
+#static dates - jack to change to be based on gui input
+startdate = date(2014,4,24)
+enddate = date(2018,4,30)
+
+#print to console all cases based on camera or radar
+
+camera = (data[data.stack().str.contains("Camera Detected").any(level=0)])
+radar = (data[data.stack().str.contains("Radar").any(level=0)])
+
+#apply date range filter
+
