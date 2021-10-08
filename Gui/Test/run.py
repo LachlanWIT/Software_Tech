eb85465aeb5de2cf6456de822b9d95f711d970a3
Archie-m1 <85227900+Archie-m1@users.noreply.github.com>
2021-10-08 12:40:37 +1000

    Update run.py

--- Gui/Test/run.py
+++ Gui/Test/run.py
@@ -5,53 +5,41 @@ import csv
 from datetime import datetime
 
 
-class gui (wx.Frame) :
+class gui(wx.Frame):
 
     def __init__(self, parent, id):
-        wx.Frame.__init__(self, parent, id, 'System Analysis and Visualisation Tool', size=(1200, 1200))
+        wx.Frame.__init__(self, parent, id, 'Traffic Analysis and Visualisation Tool', size=(1200, 1200))
         self.panel = wx.Panel(self)
 
-        getname = wx.TextEntryDialog(None,"What's you Name", "Title", 'Enter Name')
-        if getname.ShowModal()==wx.ID_OK:
-            gottenname = getname.GetValue()
+        search = wx.Button(self.panel, -1, "Search Function", (10, 150), (140, -1))  # Button will call function search
+        self.Bind(wx.EVT_BUTTON, self.entersuburbbutton, search)
 
-        self.userInput = {}
+        all = wx.Button(self.panel, -1, "All Data Function", (10, 180),(140, -1))  # Button  will call function All
+        self.Bind(wx.EVT_BUTTON, self.enterkeywordbutton, all)
 
-        customise = wx.StaticText(self.panel, -1, gottenname, (10,50), (260,50), wx.ALIGN_CENTER)
-        customise.SetSize(1200,20)
-        customise.SetForegroundColour('white')
-        customise.SetBackgroundColour('light blue')
+        keyword = wx.Button(self.panel, -1, "DCA code search", (10, 210), (140, -1))  # Button will call functionkeyword
+        self.Bind(wx.EVT_BUTTON, self.startperiodbutton, keyword)
 
-        enter_Suburb =  wx.Button(self.panel, -1, "Enter Suburb",(10,130), (140,-1))
-        self.Bind(wx.EVT_BUTTON, self.entersuburbbutton, enter_Suburb)
+        analysis = wx.Button(self.panel, -1, "Analysis Function", (10, 240),(140, -1))  # Button will call function Analysis
+        self.Bind(wx.EVT_BUTTON, self.endperiodbutton, analysis)
 
+        insight = wx.Button(self.panel, -1, "Insight Function", (10, 270),(140, -1))  # Button will call function Insight
+        self.Bind(wx.EVT_BUTTON, self.search, insight)
 
-        enter_keyword = wx.Button(self.panel, -1, "Enter Keyword", (10, 190), (140, -1))
-        self.Bind(wx.EVT_BUTTON, self.enterkeywordbutton, enter_keyword)
+        extra = wx.Button(self.panel, -1, "Future functions", (10, 300),(140, -1))  # Aditional button for future addition
+        self.Bind(wx.EVT_BUTTON, self.searchagain, extra)
 
-        search_startperiod = wx.Button(self.panel, -1, "Select Start Period", (550, 130), (140, -1))
-        self.Bind(wx.EVT_BUTTON, self.startperiodbutton, search_startperiod)
+        extra1 = wx.Button(self.panel, -1, "extra1", (10, 330),(140, -1))  # Aditional button for future addition
 
-        search_endperiod = wx.Button(self.panel, -1, "Select End Period", (850, 130), (140, -1))
-        self.Bind(wx.EVT_BUTTON, self.endperiodbutton, search_endperiod)
+        extra3 = wx.Button(self.panel, -1, "extra2", (10, 360),(140, -1))  # Button to close application
 
-        search = wx.Button(self.panel,-1, "Search", (300, 410), (500, -1))
-        self.Bind(wx.EVT_BUTTON, self.search, search)
+        clear = wx.Button(self.panel, -1, "clear", (10, 390), (140, -1))  # Button to clear screen
+        kill = wx.Button(self.panel, -1, "Close", (10, 500), (140, -1))  # Button to Close program
+        kill.Bind(wx.EVT_BUTTON, self.onClose)
 
-        searchagain = wx.Button(self.panel, -1, "Search Again", (300, 440), (500, -1))
-        self.Bind(wx.EVT_BUTTON, self.searchagain, searchagain)
-
-        showprice = wx.Button(self.panel, -1, "Show Price Distribution", (300, 470), (500, -1))
-
-        popularproperties = wx.Button(self.panel, -1, "Show Popular Properties", (300, 500), (500, -1))
-
-        cleanliness = wx.Button(self.panel, -1, "See Cleanliness Comments", (300, 530), (500, -1))
-
-
-
-    def entersuburbbutton(self,event):
+    def entersuburbbutton(self, event):
         enter = wx.TextEntryDialog(None, "Enter Suburb", "Title", "Ashgrove")
-        if enter.ShowModal()==wx.ID_OK:
+        if enter.ShowModal() == wx.ID_OK:
             self.answer = enter.GetValue()
         self.userInput['suburbs'] = self.answer
 
@@ -74,47 +62,21 @@ class gui (wx.Frame) :
         self.userInput['endDate'] = self.answerendperiod
 
     def search(self, event):
-        result = wx.StaticText(self.panel, -1, str(self.userInput), (10,600), (260,50), wx.ALIGN_CENTER)
+        result = wx.StaticText(self.panel, -1, str(self.userInput), (10, 600), (260, 50), wx.ALIGN_CENTER)
         # result.SetSize(1200,2000)
         result.SetForegroundColour('white')
         result.SetBackgroundColour('light blue')
         # print(self.userInput['suburbs'])
 
-    def searchagain(self,event):
+    def searchagain(self, event):
         self.Refresh()
 
-        
-#lachlan test function 1
-#import data
-data = pd.read_csv('penalty_data_set_2.csv', low_memory=False)
-
-#assign variable name for if statement from global button variable in GUI
-startdate = search_startperiod
-enddate = search_endperiod 
-
-
-#print to console all cases based on camera or radar
-
-camera = (data[data.stack().str.contains("Camera Detected").any(level=0)])
-radar = (data[data.stack().str.contains("Radar").any(level=0)])
-
-#apply date range filter
-
-for row in camera:
-    if camera["OFFENCE_MONTH"] >= startdate and camera["OFFENCE_MONTH"]<= enddate:
-        print(row)
-
-for row in radar:
-    if radar["OFFENCE_MONTH"] >= startdate and radar["OFFENCE_MONTH"]<= enddate:
-        print(row)
-
-#need statement to trigger this if user selects correct button option - will work with jack at uni
-        
-
+    def onClose(self, event):
+         self.Close()
 
 
 if __name__ == '__main__':
-     app = wx.PySimpleApp()
-     frame = gui(parent=None, id=1)
-     frame.Show()
-     app.MainLoop()
+    app = wx.PySimpleApp()
+    frame = gui(parent=None, id=1)
+    frame.Show()
+    app.MainLoop()
