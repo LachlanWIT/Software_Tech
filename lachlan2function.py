dd61b174c7b885bc6ce920a3684f505678b8f946
LachlanWIT <52726021+LachlanWIT@users.noreply.github.com>
2021-10-08 11:44:35 +1000

    2nd function

--- lachlan2function.py
+++ lachlan2function.py
@@ -12,34 +12,37 @@ Created on Thu Oct  7 15:54:45 2021
 #list all cases alyse the number of cases determined by specific parameters listed in the analysis tool
 #including camera type, location ect.
 
-#Test scenarios for functionaln requirements - pending for jack to add to GUI
+#Test scenarios for functional requirements - pending for jack to add to GUI
 
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
+from datetime import date
 
 #import data
 data = pd.read_csv('penalty_data_set_2.csv', low_memory=False)
 
 #inputs into gui - jack fix
-startdate = search_startperiod #assign variable to button
-enddate = search_endperiod#assign variable to buttom
+#startdate = search_startperiod #assign variable to button
+#enddate = search_endperiod#assign variable to buttom
 
 
 #print to console all cases based on camera or radar
-
-camera = (data[data.stack().str.contains("Camera Detected").any(level=0)])
+camera = (data[data.stack().str.contains("Fixed Digital Speed Camera").any(level=0)])
 radar = (data[data.stack().str.contains("Radar").any(level=0)])
-driver = (data[data.stack().str.contains("Police").any(level=0)])
-rcamera = (data[data.stack().str.contains("Red Light Camera").any(level=0)])
+mcamera = ((data[data.stack().str.contains("Mobile Digital Speed Camera").any(level=0)]))
+rcamera = (data[data.stack().str.contains("Red Light / Speed Camera").any(level=0)])
 
+#for row in camera:
+    #print(row)
 
+#for row in radar:
+    #print(row)
 
+#for row in mcamera:
+    #print(row)
 
-for row in camera:
-    if row 
+#for row in rcamera:
+    #print(row)
+
 
 
 
