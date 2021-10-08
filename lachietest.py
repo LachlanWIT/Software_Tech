853d682739baf909dd71277a86d2d46b795b1175
LachlanWIT <52726021+LachlanWIT@users.noreply.github.com>
2021-10-08 11:07:51 +1000

    a

--- lachietest.py
+++ lachietest.py
@@ -5,7 +5,6 @@ from datetime import date
 
 #import data
 data = pd.read_csv('penalty_data_set_2.csv', low_memory=False)
-
 #For a user-selected period, 
 #retrieve all cases captured by radar or camera based on offence 
 
@@ -14,7 +13,7 @@ data = pd.read_csv('penalty_data_set_2.csv', low_memory=False)
 start_date = date(2014, 1, 1)
 end_date = date(2016, 1, 1)
 
-
+#function
 def function_1(start_date, end_date):
     camera = (data[data.stack().str.contains("Camera Detected").any(level=0)])
     radar = (data[data.stack().str.contains("Radar").any(level=0)])
