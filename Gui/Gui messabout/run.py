81423b7a111e5f4832a967efe3ff519ab27b738c
Archie-m1 <85227900+Archie-m1@users.noreply.github.com>
2021-10-09 18:40:34 +1000

    Comit

--- Gui/Gui messabout/run.py
+++ Gui/Gui messabout/run.py
@@ -2,77 +2,79 @@ import wx
 import wx.grid
 
 
-
 class gui(wx.Frame):
 
     def __init__(self, parent, id):
-        wx.Frame.__init__(self, parent, id, 'Traffic Analysis and Visualisation Tool', size=(1200, 1200))
+        wx.Frame.__init__(self, parent, id, 'Traffic Analysis and Visualisation Tool', size=(800, 800))
         self.panel = wx.Panel(self)
 
-        # app icon  #this doesnt work and there is no reason it shouldnt.
-        #path = wx.IconLocation(r'icon.ico', 0)
-        #self.SetIcon(wx.Icon(path))
+        # asthetic information
+        # Title bar
+        box = wx.BoxSizer(wx.VERTICAL)
+        lbl = wx.StaticText(self.panel, -1, pos=(200, 80), style=wx.ALIGN_CENTER)
+        font = wx.Font(18, wx.ROMAN, wx.ITALIC, wx.NORMAL)
+        txt = "Traffic analysis tool"
+        lbl.SetFont(font)
+        lbl.SetLabel(txt)
+        self.Centre()
+        self.Show()
 
-        #logo
+        # logo
+        #img = wx.Image("logo.png", wx.BITMAP_TYPE_ANY)
+        #self.image = wx.StaticBitmap(self.panel, wx.ID_ANY, wx.BitmapFromImage(img), pos=(50, 50))
+
+        # app icon  #this doesnt work and there is no reason it shouldn't.
+        # path = wx.IconLocation(r'icon.ico', 0)
+        # self.SetIcon(wx.Icon(path))
+
+        # logo
         # alternate (simpler) way to load and display a jpg image from a file
         # actually you can load .jpg  .png  .bmp  or .gif files
-        logo = wx.Image('icon.ico', wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        # logo = wx.Image('icon.ico', wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         # bitmap upper left corner is in the position tuple (x, y) = (5, 5)
-        wx.StaticBitmap(self, -1, logo, (10 + logo.GetWidth(), 5), (logo.GetWidth(), logo.GetHeight()))
+        # wx.StaticBitmap(self, -1, logo, (10 + logo.GetWidth(), 5), (logo.GetWidth(), logo.GetHeight()))
 
+        # creat button
+        search = wx.Button(self.panel, -1, "Search Function", (10, 150), (140, -1))
+        displayall = wx.Button(self.panel, -1, "All Data Function", (10, 180), (140, -1))
+        keyword = wx.Button(self.panel, -1, "DCA code search", (10, 210), (140, -1))
+        CamVsRadar = wx.Button(self.panel, -1, "CamVsRadar Function", (10, 240), (140, -1))
+        insight = wx.Button(self.panel, -1, "Insight Function", (10, 270), (140, -1))
+        insightextra = wx.Button(self.panel, -1, "Future functions", (10, 300), (140, -1))
+        refresh = wx.Button(self.panel, -1, "clear", (10, 390), (140, -1))
+        kill = wx.Button(self.panel, -1, "EXIT", (10, 725), (140, -1))
 
-        search = wx.Button(self.panel, -1, "Search Function", (10, 150), (140, -1))  # Button will call function search
+        # assign task
         self.Bind(wx.EVT_BUTTON, self.keywordSearch, search)
-
-        displayall = wx.Button(self.panel, -1, "All Data Function", (10, 180),(140, -1))  # Button  will call function All
         self.Bind(wx.EVT_BUTTON, self.DistributionBttn, displayall)
-
-        keyword = wx.Button(self.panel, -1, "DCA code search", (10, 210), (140, -1))  # Button will call function keyword
         self.Bind(wx.EVT_BUTTON, self.PenaltyCases, keyword)
-
-        CamVsRadar = wx.Button(self.panel, -1, "CamVsRadar Function", (10, 240),(140, -1))  # Button will call function Analysis
         self.Bind(wx.EVT_BUTTON, self.CamVsRadar, CamVsRadar)
-        self.cb1 = wx.CheckBox(self.panel, label='Camera', pos=(10, 10))
-        self.cb2 = wx.CheckBox(self.panel, label='Radar', pos=(10, 40))
-
-
-        insight = wx.Button(self.panel, -1, "Insight Function", (10, 270),(140, -1))  # Button will call function Insight
         self.Bind(wx.EVT_BUTTON, self.insight, insight)
-
-        insightextra = wx.Button(self.panel, -1, "Future functions", (10, 300),(140, -1)) # Aditional button for future addition
         self.Bind(wx.EVT_BUTTON, self.insight, insightextra)
-
-        # extra1 = wx.Button(self.panel, -1, "extra1", (10, 330),(140, -1))  # Additional button for future addition
-
-        # extra2 = wx.Button(self.panel, -1, "extra2", (10, 360),(140, -1))  # Additional button for future addition
-
-        refresh = wx.Button(self.panel, -1, "clear", (10, 390), (140, -1))  # Button to clear screen
-
-        kill = wx.Button(self.panel, -1, "Close", (10, 500), (140, -1))  # Button to Close program
         kill.Bind(wx.EVT_BUTTON, self.onClose)
 
-        # date range
+        # date-range
+        self.label = wx.StaticText(self.panel, label="Enter start date", pos=(430, 70))
+        self.field1 = wx.TextCtrl(self.panel, value="", size=(100, 20), pos=(430, 90))
+        start = None
 
-        self.label = wx.StaticText(self.panel, label="Enter start date", pos=(310, 20))
-        self.field1 = wx.TextCtrl(self.panel, value="", size=(100, 20), pos=(310, 40))
-        self.start = None
+        self.label = wx.StaticText(self.panel, label="Enter end date", pos=(540, 70))
+        self.field2 = wx.TextCtrl(self.panel, value="", size=(100, 20), pos=(540, 90))
+        end = None
 
-        self.label = wx.StaticText(self.panel, label="Enter end date", pos=(420, 20))
-        self.field2 = wx.TextCtrl(self.panel, value="", size=(100, 20), pos=(420, 40))
-        self.end = None
+        # keyword search
+        self.label = wx.StaticText(self.panel, label="Enter Keyword", pos=(650, 70))
+        self.keyword = wx.TextCtrl(self.panel, value="", size=(100, 20), pos=(650, 90))
+        self.keyword = None
 
-        self.okbttn = wx.Button(self.panel, label="OK", id=wx.ID_OK,  pos=(450, 70))
+        # commit to variables
+        self.okbttn = wx.Button(self.panel, label="Commit", id=wx.ID_OK, pos=(675, 115))
 
-
-
-
-#date range
+    # date range
     def onOK(self, event):
-        self.start = self.field1.GetValue()
-        self.end = self.field2.GetValue()
+        start = self.field1.GetValue()
+        end = self.field2.GetValue()
         self.Destroy()
-
-
 
     def keywordSearch(self, event):
         enter = wx.TextEntryDialog(None, "Enter keyword", "Search Input", "DCA_CODE")
@@ -92,10 +94,11 @@ class gui(wx.Frame):
             self.answerstartperiod = search_startperiod.GetValue()
         self.userInput['startDate'] = self.answerstartperiod
 
-    def CamVsRadar(self, event,):
-        #how to display the output
+    def CamVsRadar(self, event, ):
+        # how to display the output
         result = wx.ScrolledCanvas(self.panel)
-
+        # self.cb1 = wx.CheckBox(self.panel, label='Camera', pos=(10, 10))
+        # self.cb2 = wx.CheckBox(self.panel, label='Radar', pos=(10, 40))
 
     def insight(self, event):
         result = wx.StaticText(self.panel, -1, str(self.userInput), (10, 600), (260, 50), wx.ALIGN_CENTER)
@@ -108,7 +111,7 @@ class gui(wx.Frame):
         self.Refresh()
 
     def onClose(self, event):
-         self.Close()
+        self.Close()
 
 
 if __name__ == '__main__':
