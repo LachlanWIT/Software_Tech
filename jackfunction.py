73a1cc37379b677e5f35fc597255431c1f04acfe
LachlanWIT <52726021+LachlanWIT@users.noreply.github.com>
2021-10-09 19:41:07 +1000

    Update jackfunction.py

--- jackfunction.py
+++ jackfunction.py
@@ -7,13 +7,10 @@ data = pd.read_csv('penalty_data_set_2.csv', low_memory=False)
 
 #get all mobile phone cases
 results = (data[data.stack().str.contains("mobile phone").any(level=0)])
-total_cases = 264608
 
-plt.plot(results, total_cases)
-plt.title('Mobile Phone cases v other cases')
-plt.xlabel("Mobile Phone Cases")
-plt.ylabel("Other cases")
-plt.show()
+
+    
+
 
 
 
